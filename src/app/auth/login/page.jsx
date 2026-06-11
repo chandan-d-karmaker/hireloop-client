421a75170f8e827967ebc4871a339e219b9d3d85
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-06-11 16:13:16 +0600

    feat: implement login page with form handling and validation

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,47 +1,50 @@
-"use client";
-
-import { authClient } from "@/lib/auth-client";
+'use client'
+import React, { useState } from 'react';
 import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
-import { Button, Card, FieldError, Form, Input, InputGroup, Label, TextField } from "@heroui/react";
-import { useState } from "react";
+import { Button, Card, FieldError, Form, Input, InputGroup, Label, TextField, toast } from "@heroui/react";
+import { authClient } from '@/lib/auth-client';
 
-export default function Signup() {
-
+const LoginPage = () => {
     const [isVisible, setIsVisible] = useState(false);
 
-    const onSubmit = async (e) => {
+        const handleSubmit = async (e) => {
         e.preventDefault();
-        const formData = new FormData(e.currentTarget);
+        const formData = new FormData(e.target);
         const userData = Object.fromEntries(formData.entries());
 
-        const { data, error } = await authClient.signUp.email({
+        const { data, error } = await authClient.signIn.email({
             ...userData,
-            callbackURL: "/",
+
+            callbackURL: '/',
         });
+        console.log("Login user data:", { data, error })
 
-        console.log("signup detailes: ", { data, error });
 
-        // alert(`Form submitted with: ${JSON.stringify(data, null, 2)}`);
+        if (error) {
+            setError(error.message);
+            return;
+        } else {
+            toast.success("Logged in successfully!")
+        }
+        // callbackURL not working, so I've to manually redirect
         window.location.href = '/'
-    };
+
+    }
+
 
     return (
-
         <div className="flex min-h-screen items-center justify-center bg-background px-4">
             <Card className="w-full max-w-md p-6 shadow-sm border border-zinc-200 dark:border-zinc-800">
 
                 {/* Header Container */}
                 <div className="flex flex-col items-center justify-center gap-1 pb-6 border-b border-zinc-100 dark:border-zinc-800 mb-6 text-center">
-                    <h1 className="text-2xl font-semibold tracking-tight text-zinc-950 dark:text-zinc-50">Create an account</h1>
+                    <h1 className="text-2xl font-semibold tracking-tight text-zinc-950 dark:text-zinc-50">
+                        Log into your account</h1>
                     <p className="text-sm text-zinc-600 dark:text-zinc-400">Fill in the fields below to get started</p>
                 </div>
 
-                <Form className="flex flex-col gap-4" onSubmit={onSubmit}>
+                <Form className="flex flex-col gap-4" onSubmit={handleSubmit}>
 
-                    <TextField className="w-full max-w-64" name="name" type="text">
-                        <Label>Name</Label>
-                        <Input placeholder="Enter your name" />
-                    </TextField>
 
                     <TextField
                         className='max-w-64 w-full'
@@ -86,15 +89,16 @@ export default function Signup() {
                     <div className="flex gap-2">
                         <Button type="submit">
                             <Check />
-                            Submit
+                            Login
                         </Button>
                         <Button type="reset" variant="secondary">
-                            Reset
+                            Cancel
                         </Button>
                     </div>
                 </Form>
             </Card>
         </div>
-
     );
-}
+};
+
+export default LoginPage;
