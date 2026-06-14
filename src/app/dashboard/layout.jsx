4b4109c78db3e950527668bb0560ae86debc825e
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-06-14 17:10:35 +0600

    feat: add padding to content area in Dashboard layout for improved spacing

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -5,7 +5,7 @@ const DashBoardLayout = ({ children }) => {
     return (
         <div className='flex min-h-screen'>
             <SideBar/>
-            <div className='flex-1'>
+            <div className='flex-1 p-4'>
                 {children}
             </div>
         </div>
